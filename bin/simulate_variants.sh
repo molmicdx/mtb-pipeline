322f0413004bbc81f21ecc62078b97f088ff0014
Yee Mey Seah <ymseah@unicorn.labmed.uw.edu>
2021-04-20 16:33:59 -0700

    Install ART and simulate reads

--- bin/simulate_variants.sh
+++ bin/simulate_variants.sh
@@ -2,6 +2,7 @@
 
 # This script creates NGS reads from a simulated variant genome
 
+VENV=mtb_amr-env
 REFERENCE_GENOME=data/GCF_000195955.2_ASM19595v2_genomic.fna
 VARIANT_SETTINGS=configs/variants_settings.conf
 VARIANT_DIR=output/variants
@@ -11,6 +12,11 @@ SINGULARITY=/molmicro/common/singularity
 GATK_IMG=gatk-4.0.8.1.simg
 R1_QUAL_PROFILE=data/649-48_m_marinum_profileR1.txt
 R2_QUAL_PROFILE=data/649-48_m_marinum_profileR2.txt
+READ_LEN=150
+AVG_READ_DEPTH=40
+MEAN_FRAG_LEN=200
+STD_DEV=10
+READS_DIR=output/reads
 
 # 1. Generate variant genome and list of introduced mutations
 echo "[variants.py] Generating variant genome..."
@@ -28,4 +34,8 @@ singularity exec -B $MNT_SINGULARITY:/mnt $SINGULARITY/$GATK_IMG gatk LeftAlignA
 echo "Done"
 
 # 4. Generate synthetic reads
+echo "[art_illumina] Simulating reads..."
+./$VENV/bin/art_illumina -1 $R1_QUAL_PROFILE -2 $R2_QUAL_PROFILE -p -sam -i $VARIANT_DIR/$1.fa -l $READ_LEN -f $AVG_READ_DEPTH -m $MEAN_FRAG_LEN -s $STD_DEV -o $READS_DIR/$1_R > $READS_DIR/$1_art_illumina.log 2<&1
+gzip $READS_DIR/$1_R*.fq
+echo "Done"
 
