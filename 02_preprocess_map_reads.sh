372b485018082e3749849fa775aa0288c9e603e8
Yee Mey Seah <ymseah@unicorn.labmed.uw.edu>
2021-04-21 15:45:38 -0700

    Remove hard-coded directory

--- 02_preprocess_map_reads.sh
+++ 02_preprocess_map_reads.sh
@@ -17,12 +17,12 @@ DEDUPED_DIR=output/deduped
 
 # 5. Trim reads
 echo "[cutadapt] Trimming reads..."
-singularity exec -B $MNT_SINGULARITY:/mnt $SINGULARITY/$CUTADAPT cutadapt --cores 8 -q 5 -b file:/mnt/NGSTYP/pipeline/Illumina_adaptors_v2.fa -B file:/mnt/NGSTYP/pipeline/Illumina_adaptors_v2.fa --minimum-length=20 -o /mnt/mtb_amr/$READS_DIR/trimmed/$1.R1.trimmed.fq.gz -p /mnt/mtb_amr/$READS_DIR/trimmed/$1.R2.trimmed.fq.gz /mnt/mtb_amr/$READS_DIR/$1_R1.fq.gz /mnt/mtb_amr/$READS_DIR/$1_R2.fq.gz > $READS_DIR/trimmed/$1_cutadapt.log 2>&1
+singularity exec -B $MNT_SINGULARITY:/mnt $SINGULARITY/$CUTADAPT cutadapt --cores 8 -q 5 -b file:/mnt/NGSTYP/pipeline/Illumina_adaptors_v2.fa -B file:/mnt/NGSTYP/pipeline/Illumina_adaptors_v2.fa --minimum-length=20 -o /mnt/mtb_amr/$READS_DIR/$1.R1.trimmed.fq.gz -p /mnt/mtb_amr/$READS_DIR/$1.R2.trimmed.fq.gz /mnt/mtb_amr/$READS_DIR/$1_R1.fq.gz /mnt/mtb_amr/$READS_DIR/$1_R2.fq.gz > $READS_DIR/$1_cutadapt.log 2>&1
 echo "Done"
 
 # 6. Map reads
 echo "[bwa mem] Mapping reads to reference genome..."
-singularity exec -B $MNT_SINGULARITY:/mnt $SINGULARITY/$BWA bwa mem /mnt/mtb_amr/$REFERENCE_GENOME /mnt/mtb_amr/$READS_DIR/trimmed/$1.R1.trimmed.fq.gz /mnt/mtb_amr/$READS_DIR/trimmed/$1.R2.trimmed.fq.gz -K 100000000 -R '@RG\tID:'$1'\tLB:LB_'$1'\tPL:illumina\tPU:TEST_RUN\tSM:NC_000962.3' > $MAPPED_DIR/$1_trimmedRG.sam
+singularity exec -B $MNT_SINGULARITY:/mnt $SINGULARITY/$BWA bwa mem /mnt/mtb_amr/$REFERENCE_GENOME /mnt/mtb_amr/$READS_DIR/$1.R1.trimmed.fq.gz /mnt/mtb_amr/$READS_DIR/$1.R2.trimmed.fq.gz -K 100000000 -R '@RG\tID:'$1'\tLB:LB_'$1'\tPL:illumina\tPU:TEST_RUN\tSM:NC_000962.3' > $MAPPED_DIR/$1_trimmedRG.sam
 echo "Done"
 
 # 7. Create sorted BAM
