2e82c04a5eeff76de11885da9cea2cd07176bc6d
Yee Mey Seah <ymseah@unicorn.labmed.uw.edu>
2021-04-22 08:34:25 -0700

    Fix singularity links

--- 01_simulate_variants.sh
+++ 01_simulate_variants.sh
@@ -7,7 +7,6 @@ REFERENCE_GENOME=data/GCF_000195955.2_ASM19595v2_genomic.fna
 VARIANT_SETTINGS=configs/variants_settings.conf
 VARIANT_DIR=output/variants
 VARIANT_NAME=$1
-MNT_SINGULARITY=/molmicro,/molmicro/working/ymseah
 SINGULARITY=/molmicro/common/singularity
 GATK=gatk-4.0.8.1.simg
 R1_QUAL_PROFILE=data/649-48_m_marinum_profileR1.txt
@@ -31,7 +30,7 @@ echo "Done"
 
 # 3. Normalize variant representation
 echo "[gatk LeftAlignAndTrimVariants] Normalizing variant representation..."
-singularity exec -B $MNT_SINGULARITY:/mnt $SINGULARITY/$GATK gatk LeftAlignAndTrimVariants -R /mnt/mtb_amr/$REFERENCE_GENOME -V /mnt/mtb_amr/$VARIANT_DIR/$1.txt.vcf -O /mnt/mtb_amr/$VARIANT_DIR/$1_normalized.vcf > $VARIANT_DIR/$1_normalized.log 2<&1
+singularity exec -B $PWD $SINGULARITY/$GATK gatk LeftAlignAndTrimVariants -R $REFERENCE_GENOME -V $VARIANT_DIR/$1.txt.vcf -O $VARIANT_DIR/$1_normalized.vcf > $VARIANT_DIR/$1_normalized.log 2<&1
 echo "Done"
 
 # 4. Generate synthetic reads
--- 02_preprocess_map_reads.sh
+++ 02_preprocess_map_reads.sh
@@ -5,7 +5,6 @@
 
 REFERENCE_GENOME=data/GCF_000195955.2_ASM19595v2_genomic.fna
 VARIANT_NAME=$1
-MNT_SINGULARITY=/molmicro,/molmicro/working/ymseah
 SINGULARITY=/molmicro/common/singularity
 CUTADAPT=cutadapt-v1.15.img
 BWA=bwa-0.7.13.img
@@ -17,28 +16,28 @@ DEDUPED_DIR=output/deduped
 
 # 5. Trim reads
 echo "[cutadapt] Trimming reads..."
-singularity exec -B $MNT_SINGULARITY:/mnt $SINGULARITY/$CUTADAPT cutadapt --cores 8 -q 5 -b file:/mnt/NGSTYP/pipeline/Illumina_adaptors_v2.fa -B file:/mnt/NGSTYP/pipeline/Illumina_adaptors_v2.fa --minimum-length=20 -o /mnt/mtb_amr/$READS_DIR/$1.R1.trimmed.fq.gz -p /mnt/mtb_amr/$READS_DIR/$1.R2.trimmed.fq.gz /mnt/mtb_amr/$READS_DIR/$1_R1.fq.gz /mnt/mtb_amr/$READS_DIR/$1_R2.fq.gz > $READS_DIR/$1_cutadapt.log 2>&1
+singularity exec -B $PWD $SINGULARITY/$CUTADAPT cutadapt --cores 8 -q 5 -b file:data/Illumina_adaptors_v2.fa -B file:data/Illumina_adaptors_v2.fa --minimum-length=20 -o $READS_DIR/$1.R1.trimmed.fq.gz -p $READS_DIR/$1.R2.trimmed.fq.gz $READS_DIR/$1_R1.fq.gz $READS_DIR/$1_R2.fq.gz > $READS_DIR/$1_cutadapt.log 2>&1
 echo "Done"
 
 # 6. Map reads
 echo "[bwa mem] Mapping reads to reference genome..."
-singularity exec -B $MNT_SINGULARITY:/mnt $SINGULARITY/$BWA bwa mem /mnt/mtb_amr/$REFERENCE_GENOME /mnt/mtb_amr/$READS_DIR/$1.R1.trimmed.fq.gz /mnt/mtb_amr/$READS_DIR/$1.R2.trimmed.fq.gz -K 100000000 -R '@RG\tID:'$1'\tLB:LB_'$1'\tPL:illumina\tPU:TEST_RUN\tSM:NC_000962.3' > $MAPPED_DIR/$1_trimmedRG.sam
+singularity exec -B $PWD $SINGULARITY/$BWA bwa mem $REFERENCE_GENOME $READS_DIR/$1.R1.trimmed.fq.gz $READS_DIR/$1.R2.trimmed.fq.gz -K 100000000 -R '@RG\tID:'$1'\tLB:LB_'$1'\tPL:illumina\tPU:TEST_RUN\tSM:NC_000962.3' > $MAPPED_DIR/$1_trimmedRG.sam
 echo "Done"
 
 # 7. Create sorted BAM
 echo "[samtools] Creating sorted BAM files..."
-singularity exec -B $MNT_SINGULARITY:/mnt $SINGULARITY/$SAMTOOLS samtools view -bo /mnt/mtb_amr/$MAPPED_DIR/$1_trimmedRG.bam /mnt/mtb_amr/$MAPPED_DIR/$1_trimmedRG.sam
-singularity exec -B $MNT_SINGULARITY:/mnt $SINGULARITY/$SAMTOOLS samtools sort /mnt/mtb_amr/$MAPPED_DIR/$1_trimmedRG.bam /mnt/mtb_amr/$MAPPED_DIR/$1_trimmedRG-sorted
+singularity exec -B $PWD $SINGULARITY/$SAMTOOLS samtools view -bo $MAPPED_DIR/$1_trimmedRG.bam $MAPPED_DIR/$1_trimmedRG.sam
+singularity exec -B $PWD $SINGULARITY/$SAMTOOLS samtools sort $MAPPED_DIR/$1_trimmedRG.bam $MAPPED_DIR/$1_trimmedRG-sorted
 echo "Done"
 
 # 8. Remove duplicate reads
 echo "[gatk Picard MarkDuplicatesWithMateCigar] Removing duplicate reads..."
-singularity exec -B $MNT_SINGULARITY:/mnt $SINGULARITY/$GATK gatk MarkDuplicatesWithMateCigar -I /mnt/mtb_amr/$MAPPED_DIR/$1_trimmedRG-sorted.bam -O /mnt/mtb_amr/$DEDUPED_DIR/$1_deduped_matecig.bam -M /mnt/mtb_amr/$DEDUPED_DIR/$1_deduped_matecig_metrics.txt --REMOVE_DUPLICATES TRUE > $DEDUPED_DIR/$1_deduped_matecig.log 2>&1
+singularity exec -B $PWD $SINGULARITY/$GATK gatk MarkDuplicatesWithMateCigar -I $MAPPED_DIR/$1_trimmedRG-sorted.bam -O $DEDUPED_DIR/$1_deduped_matecig.bam -M $DEDUPED_DIR/$1_deduped_matecig_metrics.txt --REMOVE_DUPLICATES TRUE > $DEDUPED_DIR/$1_deduped_matecig.log 2>&1
 echo "Done"
 
 # 9. Filter based on mapping quality
 echo "[samtools] Removing reads with low mapping quality..."
-singularity exec -B $MNT_SINGULARITY:/mnt $SINGULARITY/$SAMTOOLS samtools view /mnt/mtb_amr/$DEDUPED_DIR/$1_deduped_matecig.bam -q 10 -bo /mnt/mtb_amr/$DEDUPED_DIR/$1_deduped_matecig_mq10.bam
-singularity exec -B $MNT_SINGULARITY:/mnt $SINGULARITY/$SAMTOOLS samtools index /mnt/mtb_amr/$DEDUPED_DIR/$1_deduped_matecig_mq10.bam
+singularity exec -B $PWD $SINGULARITY/$SAMTOOLS samtools view $DEDUPED_DIR/$1_deduped_matecig.bam -q 10 -bo $DEDUPED_DIR/$1_deduped_matecig_mq10.ba
+singularity exec -B $PWD $SINGULARITY/$SAMTOOLS samtools index $DEDUPED_DIR/$1_deduped_matecig_mq10.bam
 echo "Done"
 
