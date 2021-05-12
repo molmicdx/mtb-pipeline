0779f6c5e7f0a71c2c242f3e347cc272b91f1c16
Yee Mey Seah <ymseah@unicorn.labmed.uw.edu>
2021-05-12 16:17:15 -0700

    Add documentation details

--- 04_check_variants.sh
+++ 04_check_variants.sh
@@ -16,10 +16,12 @@ CHECKER=bin/checker.py
 COV_LIMIT=10
 BEDTOOLS=bedtools-2.27.1-singularity-3.5.1.sif
 
-echo "Creating CSV and BED files..."
+echo "[to_csv.py] Converting mutation list to CSV file for checker.py..."
 # Convert mutation list to csv for checker.py
 python $TO_CSV $VARIANT_DIR/$1_normalized.vcf $1 --vcf
+echo "Done"
 
+echo "[to_bed.py] Converting mutation list to BED file for bedtools..."
 # Convert mutation list to bed for bedtools
 python $TO_BED $VARIANT_DIR/$1_normalized.vcf.csv
 echo "Done"
@@ -31,32 +33,32 @@ singularity exec -B $PWD $SINGULARITY/$BEDTOOLS bedtools genomecov -ibam $DEDUPE
 singularity exec -B $PWD $SINGULARITY/$BEDTOOLS bedtools intersect -a $VARIANT_DIR/$1_normalized.vcf.csv.bed -b $DEDUPED_DIR/$1_deduped_mq10_genomecov.bed -wo | cut -f 1-3,7 > $VARIANT_DIR/$1_normalized_genomecov_intersect.bed
 echo "Done"
 
-echo "Filter true mutations list..."
+echo "[filter_low_cov.py] Filter true mutations list..."
 python $FILTER_COV $VARIANT_DIR/$1_normalized_genomecov_intersect.bed $VARIANT_DIR/$1_normalized.vcf.csv $COV_LIMIT
 echo "Done"
 
 # Check true mutations
-echo "Checking GATK variant calls..."
+echo "[checker.py] Checking GATK variant calls..."
 python $CHECKER $VC_DIR/$1_mq10_gatk_normalized.vcf $VARIANT_DIR/$1_normalized.vcf.csv_covfiltered.csv $CHECKED_DIR/$1_mq10_gatk_normalized_fPOS.csv $CHECKED_DIR/$1_mq10_gatk_normalized_fNEG.csv
 mv $VC_DIR/$1_mq10_gatk_normalized_stats.csv $CHECKED_DIR
 echo "Done"
 
-echo "Checking samtools variant calls..."
+echo "[checker.py] Checking samtools variant calls..."
 python $CHECKER $VC_DIR/$1_mq10_bcftools_normalized.vcf $VARIANT_DIR/$1_normalized.vcf.csv_covfiltered.csv $CHECKED_DIR/$1_mq10_bcftools_normalized_fPOS.csv $CHECKED_DIR/$1_mq10_bcftools_normalized_fNEG.csv
 mv $VC_DIR/$1_mq10_bcftools_normalized_stats.csv $CHECKED_DIR
 echo "Done"
 
-echo "Checking FreeBayes variant calls..."
+echo "[checker.py] Checking FreeBayes variant calls..."
 python $CHECKER $VC_DIR/$1_mq10_freebayes_normalized.vcf $VARIANT_DIR/$1_normalized.vcf.csv_covfiltered.csv $CHECKED_DIR/$1_mq10_freebayes_normalized_fPOS.csv $CHECKED_DIR/$1_mq10_freebayes_normalized_fNEG.csv
 mv $VC_DIR/$1_mq10_freebayes_normalized_stats.csv $CHECKED_DIR
 echo "Done"
 
-echo "Checking VarDict variant calls..."
+echo "[checker.py] Checking VarDict variant calls..."
 python $CHECKER $VC_DIR/$1_mq10_vardict_normalized.vcf $VARIANT_DIR/$1_normalized.vcf.csv_covfiltered.csv $CHECKED_DIR/$1_mq10_vardict_normalized_fPOS.csv $CHECKED_DIR/$1_mq10_vardict_normalized_fNEG.csv
 mv $VC_DIR/$1_mq10_vardict_normalized_stats.csv $CHECKED_DIR
 echo "Done"
 
-echo "Checking DiscoSnp variant calls..."
+echo "[checker.py] Checking DiscoSnp variant calls..."
 python $CHECKER $VC_DIR/discosnp/$1_discosnp-edit_normalized_PASSsorted.vcf $VARIANT_DIR/$1_normalized.vcf.csv_covfiltered.csv $CHECKED_DIR/$1_discosnp-edit_normalized_PASSsorted_fPOS.csv $CHECKED_DIR/$1_discosnp-edit_normalized_PASSsorted_fNEG.csv
 mv $VC_DIR/discosnp/$1_discosnp-edit_normalized_PASSsorted_stats.csv $CHECKED_DIR
 echo "Done"
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ### To generate BAM files from synthetic variant genomes for testing variant callers
 
-1. Create the virtual environment by running `./create.venv.sh`
+1. Create the virtual environment by running `./create.venv.sh` and activate it with `source mtb_amr-env/bin/activate`.
 
 2. Simulate synthetic variant NGS reads by running `./01_simulate_variants.sh sample_name`
 
@@ -10,13 +10,17 @@
 
 4. Final output are `output/deduped/sample_name_deduped_mq10.bam` and `sample_name_deduped_mq10.bam.bai` for the next step of variant-calling. BAM files are also validated at this stage, and any errors can be found in `output/deduped/sample_name_deduped_mq10_validatebam.log`
 
+*These two scripts should take ~10 mins to complete.*
+
 ### To call variants with all variant callers
 
 1. Call variants by running `./03_call_variants.sh sample_name`
 
-2. Variant callers currently available: `gatk HaplotypeCaller`, `samtools/bcftools`, `freebayes`, `VarDict`, and `DiscoSnp`.
+2. Variant callers currently available in the script: `gatk HaplotypeCaller`, `samtools/bcftools`, `freebayes`, `VarDict`, and `DiscoSnp`.
 
 3. Final output is `output/called/sample_name_mq10_tool_normalized.vcf`. Output files from discosnp are contained in `output/called/discosnp/`.
+
+*This script should take ~3 hrs to complete. (Calling variants with `VarDict` takes the most time.)*
 
 ### To check called variants against list of introduced variants
 
@@ -24,3 +28,4 @@
 
 2. Final output are `output/checked/sample_name_mq10_tool_normalized_fPOS.csv`, `output/checked/sample_name_mq10_tool_normalized_fNEG.csv`, and `output/checked/sample_name_mq10_tool_normalized_stats.csv`
 
+*This script should take ~1 min to complete.*
