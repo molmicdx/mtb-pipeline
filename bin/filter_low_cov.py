cc1073c86160572b0cec607969bfdff29869a4e3
Yee Mey Seah <ymseah@unicorn.labmed.uw.edu>
2021-07-07 16:01:44 -0700

    Add read depth filtering by vcfpy

--- bin/filter_low_cov.py
+++ bin/filter_low_cov.py
@@ -1,9 +1,11 @@
 import argparse
+import vcfpy
 
-parser = argparse.ArgumentParser(description='Remove mutations with low coverage from true mutations list')
+parser = argparse.ArgumentParser(description='Filter based on read depth')
 
 parser.add_argument('bed', help='bed file with coverage information')
 parser.add_argument('true_mutations', help='csv file of true mutations')
+parser.add_argument('vcf', help='VCF from variant caller')
 parser.add_argument('cov_threshold', help='remove mutations with coverage below INT')
 args = parser.parse_args()
 
@@ -14,7 +16,7 @@ with open(args.bed, 'r') as covfile:
         # Create dictionary of position:coverage
         cov_dict[line.split('\t')[1]] = int(line.split('\t')[-1])
 
-filtered_file = args.true_mutations + '_covfiltered.csv'
+filtered_file = args.true_mutations.split('.')[0] + '_dp'+ args.cov_threshold + '.csv'
 with open(args.true_mutations, 'r') as mutation_list:
     mutations = mutation_list.readlines()
     with open(filtered_file, 'w') as outcsv:
@@ -23,5 +25,17 @@ with open(args.true_mutations, 'r') as mutation_list:
             if cov_dict[row.split(',')[1]] > int(args.cov_threshold):
                 outcsv.write(row)
 
+# adapted from https://vcfpy.readthedocs.io/en/stable/examples.html#writing-vcf-files
+# Open input, add FILTER header, and open output file
+reader = vcfpy.Reader.from_path(args.vcf)
+reader.header.add_filter_line(vcfpy.OrderedDict([
+        ('ID', 'DP' + args.cov_threshold), ('Description', 'total DP > ' + args.cov_threshold)]))
+writer = vcfpy.Writer.from_path('/dev/stdout', reader.header)
 
+# Add "DP" filter to records having more than args.cov_threshold number of reads
+for record in reader:
+    ad = sum(c.data.get('DP', 0) for c in record.calls)
+    if ad > int(args.cov_threshold):
+        record.add_filter('DP' + args.cov_threshold)
+    writer.write_record(record)
 
