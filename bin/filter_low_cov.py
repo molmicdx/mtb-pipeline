59a04291ebe23a4567ae2df4b4919e4d4b381c4f
Yee Mey Seah <ymseah@unicorn.labmed.uw.edu>
2021-07-08 12:18:28 -0700

    Remove vcfpy filtering

--- bin/filter_low_cov.py
+++ bin/filter_low_cov.py
@@ -1,11 +1,9 @@
 import argparse
-import vcfpy
 
-parser = argparse.ArgumentParser(description='Filter based on read depth')
+parser = argparse.ArgumentParser(description='Filter true mutations based on read depth')
 
 parser.add_argument('variant_bed', help='variant bed file with coverage information')
 parser.add_argument('true_mutations_csv', help='csv file of true mutations')
-parser.add_argument('vcf', help='VCF from variant caller')
 parser.add_argument('cov_threshold', help='remove mutations with coverage below INT')
 args = parser.parse_args()
 
@@ -27,18 +25,4 @@ with open(args.true_mutations_csv, 'r') as mutation_list:
             else:
                 if cov_dict[row.split(',')[1]] > int(args.cov_threshold):
                     outcsv.write(row)
-
-# adapted from https://vcfpy.readthedocs.io/en/stable/examples.html#writing-vcf-files
-# Open input, add FILTER header, and open output file
-reader = vcfpy.Reader.from_path(args.vcf)
-reader.header.add_filter_line(vcfpy.OrderedDict([
-        ('ID', 'DP' + args.cov_threshold), ('Description', 'total DP > ' + args.cov_threshold)]))
-writer = vcfpy.Writer.from_path('/dev/stdout', reader.header)
-
-# Add "DP" filter to records having more than args.cov_threshold number of reads
-for record in reader:
-    ad = sum(c.data.get('DP', 0) for c in record.calls)
-    if ad > int(args.cov_threshold):
-        record.add_filter('DP' + args.cov_threshold)
-    writer.write_record(record)
 
