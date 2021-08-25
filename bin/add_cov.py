00472c252990a05bc3f858fbdac587b424f36fbb
Yee Mey Seah <ymseah@unicorn.labmed.uw.edu>
2021-08-25 15:02:23 -0700

    Add optional parameter to get unedited BAM_DP

--- bin/add_cov.py
+++ bin/add_cov.py
@@ -7,6 +7,7 @@ parser = argparse.ArgumentParser(description='Add read depth to variants csv fil
 parser.add_argument('variants_bed', type=argparse.FileType('r'), help='variant bed file with genome coverage information')
 parser.add_argument('variants_csv', type=argparse.FileType('r'), help='csv file of variants')
 parser.add_argument('variants_cov', type=argparse.FileType('w'), help='output csv with variants and average read depths (i.e. BAM_DP)')
+parser.add_argument('--bam_dp_out', type=argparse.FileType('w'), default=None, help='output csv with positions, entry or mutation type, and average read depths (i.e. BAM_DP)')
 
 def get_read_depths(var_bed_in):
     var_reader = csv.reader(var_bed_in, delimiter='\t')
@@ -18,12 +19,13 @@ def get_read_depths(var_bed_in):
         csv_POS = int(mutation_bed[1]) + 1
         read_depth = int(mutation_bed[-2])
         num_bp = int(mutation_bed[-1])
+        entry_type = mutation_bed[3]
         lflankpos = mutation_bed[4]
         rflankpos = mutation_bed[5]
         try:
-            cov_along_mutation[csv_POS].append((read_depth, num_bp, lflankpos, rflankpos))
+            cov_along_mutation[csv_POS].append((read_depth, num_bp, lflankpos, rflankpos, entry_type))
         except KeyError:
-            cov_along_mutation[csv_POS] = [(read_depth, num_bp, lflankpos, rflankpos)]
+            cov_along_mutation[csv_POS] = [(read_depth, num_bp, lflankpos, rflankpos, entry_type)]
         mutation_bed = next(csv.reader(var_bed_in, delimiter='\t'), None)
     return cov_along_mutation
 
@@ -42,14 +44,14 @@ def get_variants_bam_dp(cov_along_mutation, var_bed_in):
         counter = 0
         while counter < len(cov_along_mutation[pos]):
             if (cov_along_mutation[pos][counter][2] == '.') or (cov_along_mutation[pos][counter][3] == '.'):
-                variants_bam_dp[pos] = calculate_bam_dp(cov_along_mutation[pos])
+                variants_bam_dp[pos] = [calculate_bam_dp(cov_along_mutation[pos]), cov_along_mutation[pos][counter][-1]]
                 counter += 1
             else:
                 lflankpos = cov_along_mutation[pos][counter][2]
                 rflankpos = cov_along_mutation[pos][counter][3]
                 left_bam_dp = calculate_bam_dp(cov_along_mutation[int(lflankpos) + 1])
                 right_bam_dp = calculate_bam_dp(cov_along_mutation[int(rflankpos) + 1])
-                variants_bam_dp[pos] = round((left_bam_dp + right_bam_dp)/2)
+                variants_bam_dp[pos] = [round((left_bam_dp + right_bam_dp)/2), cov_along_mutation[pos][counter][-1]]
                 # troubleshooting lancet edge case where lflankpos=1047759 is also chromStart for another DEL
                 # resulting in extraneous values provided to calculate_bam_dp(). Not fixed yet.
                 #if pos == 1047765:
@@ -64,15 +66,40 @@ def write_cov_to_csv(var_bam_dp, var_csv_in, cov_csv_out):
     writer.writeheader()
     variant = next(variants_reader, None)
     while variant:
-        variant['BAM_DP'] = var_bam_dp[int(variant['POS'])]
+        variant['BAM_DP'] = var_bam_dp[int(variant['POS'])][0]
         writer.writerow(variant)
         variant = next(variants_reader, None)
     return
 
+def get_original_bam_dp(cov_along_mutation, var_bed_in):
+    original_bam_dp = {}
+    for pos in cov_along_mutation.keys():
+        counter = 0
+        while counter < len(cov_along_mutation[pos]):
+            original_bam_dp[pos] = [calculate_bam_dp(cov_along_mutation[pos]), cov_along_mutation[pos][counter][-1]]
+            counter += 1
+    return original_bam_dp
+
+def write_original_bam_dp(orig_bam_dp, bamdp_csv_out):
+    entry = {}
+    fieldnames = ['POS','TYPE','BAM_DP']
+    writer = csv.DictWriter(bamdp_csv_out, fieldnames=fieldnames)
+    writer.writeheader()
+    for pos in orig_bam_dp.keys():
+        entry['POS'] = pos
+        entry['TYPE'] = orig_bam_dp[pos][1]
+        entry['BAM_DP'] = orig_bam_dp[pos][0]
+        writer.writerow(entry)
+    return
+
 def main():
     args = parser.parse_args()
-    mutation_bam_dp = get_variants_bam_dp(get_read_depths(args.variants_bed), args.variants_bed)
+    cov_at_mutation_pos = get_read_depths(args.variants_bed)
+    mutation_bam_dp = get_variants_bam_dp(cov_at_mutation_pos, args.variants_bed)
     write_cov_to_csv(mutation_bam_dp, args.variants_csv, args.variants_cov)
+    if args.bam_dp_out != None:
+        original_bam_dp = get_original_bam_dp(cov_at_mutation_pos, args.variants_bed)
+        write_original_bam_dp(original_bam_dp, args.bam_dp_out)
 
 if __name__ == '__main__':
     sys.exit(main())
