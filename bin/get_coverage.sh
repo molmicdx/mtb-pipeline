b5a441803116b2ce606544afb10c23c0affc9a6f
ymseah <ymseah@gattaca.amc.uwmedicine.org>
2021-11-09 15:04:37 -0800

    add samtools coverage with new singularity image

--- bin/get_coverage.sh
+++ bin/get_coverage.sh
@@ -6,5 +6,12 @@ REF=$2
 BINDS=/mnt/disk2/molmicro,/mnt/disk15/molmicro
 SINGULARITY=/molmicro/common/singularity
 BEDTOOLS=bedtools-2.27.1-singularity-3.5.1.sif
+SAMTOOLS=samtools_1.12.sif
 
-singularity exec -B $BINDS,$PWD $SINGULARITY/$BEDTOOLS bedtools genomecov -ibam $1 | grep $2 | awk '{ sum+=$2*$3/$4 } END { print sum }'
+# Use samtools
+echo "[samtools coverage]"
+singularity exec -B $BINDS,$PWD $SINGULARITY/$SAMTOOLS samtools coverage $1
+
+# Use bedtools
+echo "[bedtools genomecov | awk]"
+singularity exec -B $BINDS,$PWD $SINGULARITY/$BEDTOOLS bedtools genomecov -ibam $1 | grep $2 | awk '{ sum+=$2*$3/$4 } END { print "meandepth: "sum }'
