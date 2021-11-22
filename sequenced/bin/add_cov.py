b6227050e8000180b86c4babbd9b7525b48593e2
ymseah <ymseah@gattaca.amc.uwmedicine.org>
2021-11-22 11:56:34 -0800

    Add sample info to amr cov

--- sequenced/bin/add_cov.py
+++ sequenced/bin/add_cov.py
@@ -9,6 +9,7 @@ parser.add_argument('variants_bed', type=argparse.FileType('r'), help='variant b
 parser.add_argument('variants_csv', type=argparse.FileType('r'), help='csv file of variants')
 parser.add_argument('variants_cov', type=argparse.FileType('w'), help='output csv with variants and average read depths (i.e. BAM_DP)')
 parser.add_argument('--summstats_csv', type=argparse.FileType('w'), default=None, help='output csv with read depths summary statistics (i.e. BAM_DP')
+parser.add_argument('--samplename', default='', help='isolate name (string)')
 parser.add_argument('--bam_dp_out', type=argparse.FileType('w'), default=None, help='output csv with positions, entry or mutation type, and average read depths (i.e. BAM_DP)')
 
 def get_read_depths(var_bed_in):
@@ -31,10 +32,14 @@ def get_read_depths(var_bed_in):
         mutation_bed = next(csv.reader(var_bed_in, delimiter='\t'), None)
     return cov_along_mutation
 
-def get_summstats(read_depth_list):
+def get_read_depths_per_site(read_depth_list):
     read_depths = []
     for entry in read_depth_list:
         read_depths += [entry[0]] * entry[1]
+    return read_depths
+
+def get_summstats(read_depth_list):
+    read_depths = get_read_depths_per_site(read_depth_list)
     min_bam_dp = min(read_depths)
     max_bam_dp = max(read_depths)
     mean_bam_dp = round(np.mean(read_depths))
@@ -42,15 +47,16 @@ def get_summstats(read_depth_list):
     qt_bam_dp = np.quantile(read_depths, [0.25, 0.5, 0.75])
     return min_bam_dp, max_bam_dp, mean_bam_dp, sd_bam_dp, qt_bam_dp
 
-def write_summstats(loci_cov, summstats_csv):
+def write_summstats(loci_cov, summstats_csv, samplename=''):
     loci_summstat = {}
     for pos in loci_cov.keys():
         loci_summstat[pos] = [get_summstats(loci_cov[pos]), loci_cov[pos][0][-1]]
-    fieldnames = ['POS','TYPE','MIN_BAM_DP','MAX_BAM_DP','BAM_DP','SD_BAM_DP','1QT_BAM_DP','MED_BAM_DP','3QT_BAM_DP']
+    fieldnames = ['SAMPLE', 'POS','TYPE','MIN_BAM_DP','MAX_BAM_DP','BAM_DP','SD_BAM_DP','1QT_BAM_DP','MED_BAM_DP','3QT_BAM_DP']
     writer = csv.DictWriter(summstats_csv, fieldnames=fieldnames)
     writer.writeheader()
     for pos in loci_summstat.keys():
         locus = {}
+        locus['SAMPLE'] = samplename
         locus['POS'] = pos
         locus['TYPE'] = loci_summstat[pos][-1]
         locus['MIN_BAM_DP'] = loci_summstat[pos][0][0]
@@ -94,7 +100,6 @@ def get_variants_bam_dp(cov_along_mutation, var_bed_in):
                 break
     return variants_bam_dp
 
-
 def write_cov_to_csv(var_bam_dp, var_csv_in, cov_csv_out):
     variants_reader = csv.DictReader(var_csv_in)
     writer = csv.DictWriter(cov_csv_out, fieldnames = variants_reader.fieldnames)
@@ -136,7 +141,7 @@ def main():
         original_bam_dp = get_original_bam_dp(cov_at_mutation_pos, args.variants_bed)
         write_original_bam_dp(original_bam_dp, args.bam_dp_out)
     if args.summstats_csv != None:
-        write_summstats(cov_at_mutation_pos, args.summstats_csv)
+        write_summstats(cov_at_mutation_pos, args.summstats_csv, args.samplename)
 
 if __name__ == '__main__':
     sys.exit(main())
